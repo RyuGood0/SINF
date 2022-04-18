7e11d1e9f35c761282f296d02d5a1abeeba1f529
RyuGood0 <ryugood0@gmail.com>
2022-04-18 17:22:17 +0200

    Algèbre

--- devoir.py
+++ devoir.py
@@ -1,10 +1,14 @@
 def multiply(A,B):
+    # Vérifie que les deux matrices sont de bonnes dimensions
     if type(A[0]) != list or type(B[0]) != list: raise Exception('Dimension mismatch')
     if len(A[0]) != len(B): raise Exception('Dimension mismatch')
+
+    # Initialise la matrice résultat
     m = len(A)
     n = len(B[0])
     out = [[0]*n for i in range(m)]
 
+    # Multiplie les matrices
     for i in range(m):
         for j in range(n):
             for k in range(len(B)):
